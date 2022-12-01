5ec413c0b40b9e95ff48dcdf5fb9e88ef9fb3fc9
Timon Sarakinis <timon.sarakinis@volvocars.com>
2022-12-01 17:33:04 +0100

    dayt01(part1&2): added solutions for day01
    solution for day01 in kotlin

--- day01/src/main/kotlin/Day01.kt
+++ day01/src/main/kotlin/Day01.kt
@@ -1,20 +1,42 @@
 import java.io.File
 
 fun main() {
-    val input = File("input.txt").readLines()
-        .map { it.toInt() }
-
+    val input = File("input.txt").readLines().toMutableList()
+    input.add("")
     val answer = when (System.getenv("part")) {
         "part2" -> solutionPart2(input)
-        else -> solutionPart1(input)
+        else -> solutionPart2(input)
     }
     println(answer)
 }
 
-private fun solutionPart1(input: List<Int>): Int {
-    return input[0]
+private fun solutionPart1(input: MutableList<String>): Int {
+    return createElfes(input).sortedBy { it.getTotCal() }.last().getTotCal()
 }
 
-private fun solutionPart2(input: List<Int>): Int {
-    return input[1]
+private fun solutionPart2(input: List<String>): Int {
+    return createElfes(input).sortedBy { it.getTotCal() }.takeLast(3).map { it.getTotCal() }.sum()
+}
+
+private fun createElfes(input: List<String>): MutableList<Elf> {
+    val elfes = mutableListOf<Elf>()
+    var elf = Elf()
+    for (row in input) {
+        if (row.isBlank()) {
+            elfes.add(elf)
+            elf = Elf()
+        } else
+            elf.addFood(row.toInt())
+    }
+    return elfes
+}
+
+class Elf {
+    private val food = mutableListOf<Int>()
+    fun addFood(cal: Int) {
+        food.add(cal)
+    }
+    fun getTotCal(): Int {
+        return food.sum()
+    }
 }
