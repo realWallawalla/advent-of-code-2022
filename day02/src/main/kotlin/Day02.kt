39efe17ea53fe59103104b4c7176d9d29c2d50fd
Timon Sarakinis <timon.sarakinis@volvocars.com>
2022-12-02 18:49:31 +0100

    day02 part2 solved
    solved with kotlin

--- day02/src/main/kotlin/Day02.kt
+++ day02/src/main/kotlin/Day02.kt
@@ -10,14 +10,11 @@ fun main() {
 }
 
 private fun solutionPart1(input: List<String>): Int {
-    return input.map { round -> calcScore(round) }.sum()
-}
-
-fun calcScore(signs: String): Int {
     val scoreTable = mapOf("AY" to 8, "AX" to 4, "AZ" to 3,  "BX" to 1, "BY" to 5, "BZ" to 9, "CX" to 7, "CY" to 2, "CZ" to 6)
-    return scoreTable.getValue(signs)
+    return input.map { round -> scoreTable.getValue(round) }.sum()
 }
 
 private fun solutionPart2(input: List<String>): Int {
-    return 2
+    val scoreTable = mapOf("AY" to 4, "AX" to 3, "AZ" to 8,  "BX" to 1, "BY" to 5, "BZ" to 9, "CX" to 2, "CY" to 6, "CZ" to 7)
+    return input.map { round -> scoreTable.getValue(round) }.sum()
 }
