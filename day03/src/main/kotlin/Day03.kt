f73582950dee3858bef72b92a7feb358219c832d
Timon Sarakinis <timon.sarakinis@volvocars.com>
2022-12-03 15:05:28 +0100

    day03 part 1 solution
    solved with kotlin

--- day03/src/main/kotlin/Day03.kt
+++ day03/src/main/kotlin/Day03.kt
@@ -2,17 +2,21 @@ import java.io.File
 
 fun main() {
     val input = File("input.txt").readLines()
-    val answer = when (System.getenv("part")) {
-        "part2" -> solutionPart2(input)
-        else -> solutionPart1(input)
-    }
+    val answer = when (System.getenv("part")) { "part2" -> solutionPart2(input) else -> solutionPart1(input) }
     println(answer)
 }
 
 private fun solutionPart1(input: List<String>): Int {
-    return 1
+    val prioTable = ('a'..'z').associateWith { it - 'a' + 1 }.toMutableMap()
+    prioTable.putAll(('A'..'Z').associateWith { it - 'A' + 27 })
+    val rackSack = input.map { s -> listOf(s.substring(0, Math.floorDiv(s.length, 2)), s.substring(Math.floorDiv(s.length, 2), s.length)) }
+    return rackSack.map { comps -> comps[0].firstOrNull { item -> comps[1].contains(item) } }.sumOf { duplicate -> prioTable.getValue(duplicate!!)}
 }
 
 private fun solutionPart2(input: List<String>): Int {
     return 2
+}
+
+class Racksack(comp1: CharSequence, comp2: CharSequence) {
+
 }
