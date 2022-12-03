b8010bd4b63a7f068f7bc8b66f88aec6ea95328b
Timon Sarakinis <timon.sarakinis@volvocars.com>
2022-12-03 11:16:29 +0100

    day02 optimizes solution
    removed some filtering

--- day02/src/main/kotlin/Day02.kt
+++ day02/src/main/kotlin/Day02.kt
@@ -1,20 +1,20 @@
 import java.io.File
 
 fun main() {
-    val input = File("input.txt").readLines().map { it.filter { !it.isWhitespace() } }
+    val input = File("input.txt").readLines()
     val answer = when (System.getenv("part")) {
         "part2" -> solutionPart2(input)
-        else -> solutionPart1(input)
+        else -> solutionPart2(input)
     }
     println(answer)
 }
 
 private fun solutionPart1(input: List<String>): Int {
-    val scoreTable = mapOf("AY" to 8, "AX" to 4, "AZ" to 3,  "BX" to 1, "BY" to 5, "BZ" to 9, "CX" to 7, "CY" to 2, "CZ" to 6)
+    val scoreTable = mapOf("A Y" to 8, "A X" to 4, "A Z" to 3,  "B X" to 1, "B Y" to 5, "B Z" to 9, "C X" to 7, "C Y" to 2, "C Z" to 6)
     return input.map { round -> scoreTable.getValue(round) }.sum()
 }
 
 private fun solutionPart2(input: List<String>): Int {
-    val scoreTable = mapOf("AY" to 4, "AX" to 3, "AZ" to 8,  "BX" to 1, "BY" to 5, "BZ" to 9, "CX" to 2, "CY" to 6, "CZ" to 7)
+    val scoreTable = mapOf("A Y" to 4, "A X" to 3, "A Z" to 8,  "B X" to 1, "B Y" to 5, "B Z" to 9, "C X" to 2, "C Y" to 6, "C Z" to 7)
     return input.map { round -> scoreTable.getValue(round) }.sum()
 }
