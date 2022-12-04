04f5c0d852c63f5d61a301d54c7ec83413419875
Timon Sarakinis <timon.sarakinis@volvocars.com>
2022-12-04 13:30:57 +0100

    day04 part2 solution
    solved with kotlin

--- day04/src/main/kotlin/Day04.kt
+++ day04/src/main/kotlin/Day04.kt
@@ -1,21 +1,21 @@
 import java.io.File
 
 fun main() {
-    val input = File("input.txt").readLines()
+    val input = File("input.txt").readLines().map { it.split(",") }.map { apair ->
+        apair.map { section -> Range(section.substringBefore("-").toInt(), section.substringAfter("-").toInt()) } }
     val answer = when (System.getenv("part")) {"part2" -> solutionPart2(input) else -> solutionPart1(input) }
     println(answer)
 }
 
-private fun solutionPart1(input: List<String>): Int {
-    return input.map { it.split(",") }.filter { apair -> Range(apair[0].substringBefore("-").toInt(),
-        apair[0].substringAfter("-").toInt()).rangeContainedInOtherRange(Range(apair[1].substringBefore("-").toInt(),
-        apair[1].substringAfter("-").toInt())) }.count()
+private fun solutionPart1(input: List<List<Range>>): Int {
+    return input.filter { pair -> pair[0].rangeContainedInOtherRange(pair[1]) }.count()
 }
 
-private fun solutionPart2(input: List<String>): Int {
-    return 2
+private fun solutionPart2(input: List<List<Range>>): Int {
+    return input.filter { pair -> pair[0].rangeOverlap(pair[1]) }.count()
 }
 
 class Range(val min: Int, val max: Int) {
     fun rangeContainedInOtherRange(range: Range): Boolean = (this.min >= range.min && this.max <= range.max) || (this.min <= range.min && this.max >= range.max)
+    fun rangeOverlap(range: Range): Boolean = (this.max >= range.min) && (this.min <= range.max)
 }
