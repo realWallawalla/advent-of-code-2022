de9c63acdfc21cfae5c6835fde7c026c7c03647f
Timon Sarakinis <timon.sarakinis@volvocars.com>
2022-12-04 12:33:15 +0100

    day04 part 1 solution
    solved with kotlin

--- day04/src/main/kotlin/Day04.kt
+++ day04/src/main/kotlin/Day04.kt
@@ -2,15 +2,20 @@ import java.io.File
 
 fun main() {
     val input = File("input.txt").readLines()
-    val answer = when (System.getenv("part")) {
-        "part2" -> solutionPart2(input) else -> solutionPart1(input) }
+    val answer = when (System.getenv("part")) {"part2" -> solutionPart2(input) else -> solutionPart1(input) }
     println(answer)
 }
 
 private fun solutionPart1(input: List<String>): Int {
-    return 1
+    return input.map { it.split(",") }.filter { apair -> Range(apair[0].substringBefore("-").toInt(),
+        apair[0].substringAfter("-").toInt()).rangeContainedInOtherRange(Range(apair[1].substringBefore("-").toInt(),
+        apair[1].substringAfter("-").toInt())) }.count()
 }
 
 private fun solutionPart2(input: List<String>): Int {
     return 2
+}
+
+class Range(val min: Int, val max: Int) {
+    fun rangeContainedInOtherRange(range: Range): Boolean = (this.min >= range.min && this.max <= range.max) || (this.min <= range.min && this.max >= range.max)
 }
