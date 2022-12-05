93edf934d19b3a8e08a7b063cbed41efa1c45c8b
Timon Sarakinis <timon.sarakinis@volvocars.com>
2022-12-05 09:56:19 +0100

    day05 preparing with kotlin gradle
    setting up project for day05

---
+++ day05/src/main/kotlin/Day05.kt
@@ -0,0 +1,15 @@
+import java.io.File
+
+fun main() {
+    val input = File("input.txt").readLines()
+    val answer = when (System.getenv("part")) {"part2" -> solutionPart2(input) else -> solutionPart1(input) }
+    println(answer)
+}
+
+private fun solutionPart1(input: List<String>): Int {
+    return 1
+}
+
+private fun solutionPart2(input: List<String>): Int {
+    return 2
+}
